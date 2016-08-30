f706f4f6ceafd90dbddf736700a68b7ab35a4999
ocefpaf <ocefpaf@gmail.com>
2016-08-30 13:59:15 -0300

    rerender to get yum

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -46,7 +46,7 @@ source run_conda_forge_build_setup
 # "recipe/yum_requirements.txt" file. After updating that file,
 # run "conda smithy rerender" and this line be updated
 # automatically.
-yum install -y liburiparser-devel
+yum install -y ImageMagick dos2unix fop libxslt
 
 
 # Embarking on 1 case(s).
