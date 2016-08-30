07f10db174d68b3a1de08d2ce3720400f0379896
ivoflipse <ivoflipse@gmail.com>
2016-08-30 13:12:58 +0200

    Added toolchain + hack to activate env

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# FIXME: This is a hack to make sure the environment is activated.
+# The reason this is required is due to the conda-build issue
+# mentioned below.
+#
+# https://github.com/conda/conda-build/issues/910
+#
+source activate "${CONDA_DEFAULT_ENV}"
+
 # Steps from: http://miktex.org/howto/build-unx
 # Any args for CMAKE?
 cmake -G "Unix Makefiles"
