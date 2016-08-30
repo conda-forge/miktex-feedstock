58d0b54ff4a283a6b3a8bc1c2715b1af332de332
ivoflipse <ivoflipse@gmail.com>
2016-08-30 13:10:18 +0200

    Pinned versions
    Pimped build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+# Steps from: http://miktex.org/howto/build-unx
+# Any args for CMAKE?
 cmake -G "Unix Makefiles"
 make
-sudo make install
-sudo initexmf --admin --configure
-sudo mpm --admin --update-db
-initexmf --update-fndb
+# Use ninja instead?
+make install
+
+# These steps are optional
+# You use the MiKTeX configuration utility (initexmf) and the MiKTeX package manager (mpm) to configure "MiKTeX Tools" system-wide
+# initexmf --admin --configure
+# mpm --admin --update-db
+# Once you have configured MiKTeX for the system, each user should create the per-user file name database
+# initexmf --update-fndb
