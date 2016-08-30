6f649aeefc9c0dcfdc2c154610ee04923d36b2c2
ivoflipse <ivoflipse@gmail.com>
2016-08-30 13:51:12 +0200

    Modified run_docker_build.sh

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -11,17 +11,12 @@ RECIPE_ROOT=$FEEDSTOCK_ROOT/recipe
 docker info
 
 config=$(cat <<CONDARC
-
 channels:
  - conda-forge
-
  - defaults # As we need conda-build
-
 conda-build:
  root-dir: /feedstock_root/build_artefacts
-
 show_channel_urls: true
-
 CONDARC
 )
 
@@ -43,20 +38,44 @@ conda update --yes --all
 conda install --yes conda-build
 conda info
 
-# Embarking on 3 case(s).
+# Embarking on 6 case(s).
     set -x
+    export CONDA_NPY=110
     export CONDA_PY=27
     set +x
     conda build /recipe_root --quiet || exit 1
     /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
 
     set -x
+    export CONDA_NPY=111
+    export CONDA_PY=27
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
+
+    set -x
+    export CONDA_NPY=110
     export CONDA_PY=34
     set +x
     conda build /recipe_root --quiet || exit 1
     /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
 
     set -x
+    export CONDA_NPY=111
+    export CONDA_PY=34
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
+
+    set -x
+    export CONDA_NPY=110
+    export CONDA_PY=35
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
+
+    set -x
+    export CONDA_NPY=111
     export CONDA_PY=35
     set +x
     conda build /recipe_root --quiet || exit 1
